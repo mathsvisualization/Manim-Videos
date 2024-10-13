34cf6c31054f3c9b58ac2355870a677f6a053156
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2024-10-13 08:47:12 +0530

    Create lru_cache.py

---
+++ _custom/lru_cache.py
@@ -0,0 +1,17 @@
+from functools import lru_cache, cache
+
+@lru_cache
+def fib(n: int) -> int:
+    if n<=1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+        
+for i in range(0, 100):
+    print(f"{i}: {fib(i)}")
+    
+@cache
+def factorial(n):
+    return n * factorial(n-1) if n else 1
+    
+[print(factorial(i)) for i in range(6)]
