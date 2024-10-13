201447373a35112ad001a820d40061bed0bb82e5
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2024-10-13 08:48:13 +0530

    Delete Total_Classes_Print.py

--- Total_Classes_Print.py
+++
@@ -1,8 +0,0 @@
-import custom 
-
-# Get the list of methods in manim
-method = dir(custom)
-
-# Save the methods to a text file
-with open("manim_methods.txt", "w") as file:
-    file.write("\n".join(method))
